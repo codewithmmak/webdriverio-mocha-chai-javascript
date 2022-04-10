2bec198f36198c8b98a5a06c88a6aaec0133ed84
qaloop <agiletester786@gmail.com>
2022-04-10 09:30:43 +0530

    Updated packages

--- test/specs/login.test.js
+++ test/specs/login.test.js
@@ -15,7 +15,7 @@ describe('Login Tests ', function () {
         loginPage.enterPassword('SuperSecretPassword!')
         assert.equal('SuperSecretPassword!', loginPage.password.getValue())
         loginPage.clickLoginBtn()
-        browser.pause(3000)
+        browser.pause(1000)
         assert.equal('You logged into a secure area!\n×', secureAreaPage.flashMessage.getText())
         assert.equal('Secure Area', secureAreaPage.h2Header.getText())
         assert.equal('Welcome to the Secure Area. When you are done click logout below.', secureAreaPage.h4Header.getText())
